b228a38094e88445800cbd79ef4e95b9df5c437f
Jeetaksh25 <manojjeetakshluhaniwal25@gmail.com>
2025-03-03 03:02:57 +0530

    Sign Out

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
+import { signOut } from '../../lib/appwrite';
+import AlertBox from '../../comps/AlertBox';
+import CustomButton from '../../comps/CustomButton';
+import { account } from '../../lib/appwrite';
+import { router } from 'expo-router';
+import { useRouter } from 'expo-router';
 
 const Profile = () => {
+
+  const router = useRouter();
+
+  const [isSigningOut, setIsSigningOut] = useState(false);
+
+  const [alert, setAlert] = useState<{
+      type: "error" | "success" | "muted" | "warning" | "info";
+      message: string;
+    } | null>(null);
+  
+    const showAlert = (
+      type: "error" | "success" | "muted" | "warning" | "info",
+      message: string
+    ) => {
+      setAlert({ type, message });
+  
+      setTimeout(() => {
+        setAlert(null);
+      }, 3000);
+    };
+
+    const handleSignOut = async () => {
+      try {
+        setIsSigningOut(true);
+    
+        const user = await account.get().catch(() => null);
+        if (!user) {
+          showAlert("warning", "No user is signed in");
+          router.push("/");
+          return;
+        }
+    
+        await signOut();
+        showAlert("success", "Signed out successfully");
+
+        setTimeout(()=>{
+          router.push("/");
+        },1000)
+
+      } catch (error) {
+        console.log(error);
+        showAlert("error", "Something went wrong while signing out");
+      } finally {
+        setIsSigningOut(false);
+      }
+    };
+    
+
   return (
     <View>
       <Text>Profile</Text>
+      <CustomButton title="Sign Out" loadingText='Signing Out...' isLoading={isSigningOut} handlePress={handleSignOut}></CustomButton>
+      {alert && <AlertBox actionText={alert.type} desc={alert.message} />}
     </View>
   )
 }
