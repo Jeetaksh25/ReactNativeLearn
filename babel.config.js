3e66efe7c6f2f071ea0defaeefa3e9f39a7e1b0e
Jeetaksh25 <manojjeetakshluhaniwal25@gmail.com>
2025-04-04 23:47:49 +0530

    .env added

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,39 @@
-module.exports = function(api) {
+module.exports = function (api) {
     api.cache(true);
-
+  
     return {
-        presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
-        }], "nativewind/babel"],
-
-        plugins: [["module-resolver", {
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel"
+      ],
+      plugins: [
+        [
+          "module-resolver",
+          {
             root: ["./"],
-
             alias: {
-                "@": "./",
-                "tailwind.config": "./tailwind.config.js"
+              "@": "./",
+              "tailwind.config": "./tailwind.config.js"
             }
-        }]]
+          }
+        ],
+        [
+          "module:react-native-dotenv",
+          {
+            moduleName: "@env",
+            path: ".env",
+            allowlist: [
+              "APPWRITE_ENDPOINT",
+              "APPWRITE_PLATFORM",
+              "APPWRITE_PROJECT_ID",
+              "APPWRITE_DATABASE_ID",
+              "APPWRITE_USER_COLLECTION_ID",
+              "APPWRITE_VIDEO_COLLECTION_ID",
+              "APPWRITE_STORAGE_ID"
+            ]
+          }
+        ]
+      ]
     };
-};
+  };
+  
